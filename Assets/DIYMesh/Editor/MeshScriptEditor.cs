514dde47dcbb5956e7c402b3450978e1a1c806b8
Mauve <keqing.hu@icloud.com>
2019-12-28 10:30:24 +0800

    feat(MeshDiy): add more variable to set the object

--- Assets/DIYMesh/Editor/MeshScriptEditor.cs
+++ Assets/DIYMesh/Editor/MeshScriptEditor.cs
@@ -12,9 +12,7 @@ namespace DIYMesh.Editor
         private SerializedProperty _mode;
 
         private DiyMeshObject _this;
-
         private Color _color = Color.white;
-        // float a = 0, b = 0;
 
         public void OnEnable()
         {
@@ -24,41 +22,64 @@ namespace DIYMesh.Editor
 
         public override void OnInspectorGUI()
         {
-//            _isDemo = EditorGUILayout.Popup("Mode", _isDemo, new[] {"Demo", "Object"});
             var screenSize = new Vector2(Screen.width, Screen.height);
             EditorGUILayout.PropertyField(_drawMode);
             EditorGUILayout.PropertyField(_mode);
             serializedObject.ApplyModifiedProperties();
             if (_mode.enumValueIndex == 1) return;
-            (_this = (DiyMeshObject) target).drawCoolDown = EditorGUILayout.Slider("Draw Cool Down", _this.drawCoolDown, 0.05f, 1f);
+            (_this = (DiyMeshObject) target).drawCoolDown =
+                EditorGUILayout.Slider("Draw Cool Down", _this.drawCoolDown, 0.05f, 1f);
+            // Rect rect = EditorGUILayout.GetControlRect(true);
+            // Debug.Log(rect);
+            EditorGUILayout.LabelField("Draw Weight");
             if (_this.threeDimensional)
             {
                 EditorGUILayout.BeginHorizontal();
                 EditorGUILayout.LabelField(_this.drawBehindWeight.ToString(CultureInfo.CurrentCulture),
-                    GUILayout.Width(screenSize.x / 7));
+                    GUILayout.Width(screenSize.x * 2 / 15));
                 EditorGUILayout.MinMaxSlider(ref _this.drawBehindWeight,
-                    ref _this.drawForwardWeight, -1, 1, GUILayout.Width(screenSize.x * 4 / 7));
+                    ref _this.drawForwardWeight, -1, 1, GUILayout.Width(screenSize.x * 10 / 15));
                 EditorGUILayout.LabelField(_this.drawForwardWeight.ToString(CultureInfo.CurrentCulture),
-                    GUILayout.Width(screenSize.x / 7));
+                    GUILayout.Width(screenSize.x * 2 / 15));
                 EditorGUILayout.EndHorizontal();
-                ;
             }
             else
             {
                 _this.drawForwardWeight =
-                    EditorGUILayout.Slider(_this.drawForwardWeight.ToString(CultureInfo.CurrentCulture), _this.drawForwardWeight, -1, 1);
+                    EditorGUILayout.Slider(_this.drawForwardWeight.ToString(CultureInfo.CurrentCulture),
+                        _this.drawForwardWeight, -1, 1);
             }
 
-            // EditorGUILayout.MinMaxSlider(ref a,  ref b, -10, 10);
             serializedObject.ApplyModifiedProperties();
             _color = EditorGUILayout.ColorField("Color", _color);
-
-            if (!Application.isPlaying) return;
-            _this.SetColor(_color, _color.a < 1 ? RenderingMode.Fade : RenderingMode.Opaque);
-            if (GUILayout.Button("Clear"))
+            if (Application.isPlaying)
             {
-                _this.Clear();
+                _this.SetColor(_color, _color.a < 1 ? RenderingMode.Fade : RenderingMode.Opaque);
             }
+
+            EditorGUILayout.BeginHorizontal();
+            if (Application.isPlaying)
+            {
+                if (GUILayout.Button("Clear"))
+                {
+                    _this.Clear();
+                }
+            }
+
+            if (_this.threeDimensional)
+            {
+                if (GUILayout.Button("Reset Left Value"))
+                {
+                    _this.drawBehindWeight = 0;
+                }
+            }
+
+            if (GUILayout.Button(_this.threeDimensional ? "Reset Right Value" : "Reset Value"))
+            {
+                _this.drawForwardWeight = 0;
+            }
+
+            EditorGUILayout.EndHorizontal();
         }
     }
 }
