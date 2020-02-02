1b63a157d866241a50e0cb05b18f55a393714b78
HuKeqing <keqing.hu@icloud.com>
2020-02-02 23:25:14 +0800

    feat(Event): unity f event

---
+++ Assets/Event/EventDemo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+namespace Event
+{
+    public class EventDemo : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log("Awake");
+        }
+
+        private void Reset()
+        {
+            Debug.Log("Reset");
+        }
+
+        private void Start()
+        {
+            Debug.Log("Start");
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log("OnDisable");
+        }
+
+        private void OnEnable()
+        {
+            Debug.Log("OnEnable");
+        }
+
+        private void OnMouseDrag()
+        {
+            Debug.Log("OnMouseDrag");
+        }
+
+        private void OnBecameInvisible()
+        {
+            Debug.Log("OnBecameInvisible");
+        }
+
+        private void OnBecameVisible()
+        {
+            Debug.Log("OnBecameVisible");
+        }
+
+        private void OnMouseDown()
+        {
+            Debug.Log("OnMouseDown");
+        }
+
+        private void OnMouseEnter()
+        {
+            Debug.Log("OnMouseEnter");
+        }
+
+        private void OnMouseExit()
+        {
+            Debug.Log("OnMouseExit");
+        }
+
+        private void OnMouseOver()
+        {
+            Debug.Log("OnMouseOver");
+        }
+
+        private void OnMouseUp()
+        {
+            Debug.Log("OnMouseUp");
+        }
+    }
+}
