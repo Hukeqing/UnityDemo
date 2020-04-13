0295889d0b77472914c68d63ea969acbe7292c8f
HuKeqing <keqing.hu@icloud.com>
2020-04-13 21:13:46 +0800

    feat(Jump): Add the jump Demo by curve

---
+++ Assets/Jump/JumpScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Jump
+{
+    public class JumpScript : MonoBehaviour
+    {
+        public AnimationCurve jumpCurve;
+        
+        private bool _isJump;
+        private float _jumpStatus;
+        private Vector3 _baseVector3;
+
+        private void Update()
+        {
+            if (!_isJump && Input.GetKeyDown(KeyCode.Space))
+            {
+                _isJump = true;
+                _jumpStatus = 0;
+                _baseVector3 = transform.position;
+            }
+
+            if (!_isJump) return;
+            _jumpStatus += Time.deltaTime;
+            if (_jumpStatus >= 1.0f)
+            {
+                _isJump = false;
+                transform.position = _baseVector3;
+            }
+
+            transform.position = _baseVector3 + jumpCurve.Evaluate(_jumpStatus) * Vector3.up;
+        }
+    }
+}
